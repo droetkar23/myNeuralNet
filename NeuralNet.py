770994b6a156a9a99e02c7f9d032e118b83c5874
droetkar23 <n.tesla@gmx.net>
2021-03-01 12:50:25 +0100

    commit 5

--- NeuralNet.py
+++ NeuralNet.py
@@ -28,8 +28,8 @@ class NeuralNet:
 
     def think(self, input_layer):
 
-        if not (len(input_layer[0]) == self.layer_sizes[0]):
-            print('The input does not match the neural net.')
+        # if not (len(input_layer[0]) == self.layer_sizes[0]):
+        #     print('The input does not match the neural net.')
 
         self.activations[0] = input_layer
 
@@ -37,20 +37,22 @@ class NeuralNet:
             self.activations[i + 1] = sigmoid(np.dot(a, w) + b)
         return self.activations[-1]
 
-    def gradient(self):
+    def gradient(self, training_example):
 
         nabla_w = [np.zeros(w.shape) for w in self.weights]
         nabla_b = [np.zeros(b.shape) for b in self.biases]
         z = [np.dot(a, w) + b for a, w, b in zip(self.activations, self.weights, self.biases)]
 
-        nabla_a = self.activations[-1]
-        print('nabla_a = output_layer')
-        print(nabla_a.shape, '=', self.activations[-1].shape)
+
+
+        nabla_a = self.activations[-1]-training_example
+        # print('nabla_a = output_layer')
+        # print(nabla_a.shape, '=', self.activations[-1].shape)
 
         for a, w, z, n_w, n_b in zip(self.activations[-2::-1], self.weights[::-1], z[::-1],
                                      nabla_w[::-1], nabla_b[::-1]):
 
-            nabla_w = np.outer(a, sigmoid_derivative(z) * nabla_a)
+            nabla_w = np.outer(a, sigmoid_derivative(z) * nabla_a)    ###############!!!!!!!!!!!!!! da stimmt was nicht!
             nabla_b = sigmoid_derivative(z) * nabla_a
             nabla_a = np.inner(sigmoid_derivative(z) * nabla_a, w)
 
@@ -61,20 +63,38 @@ class NeuralNet:
 
         return nabla_w, nabla_b
 
-    def learn(self, learning_rate: float = 0.1, epochs: int = 1):
+    def learn(self, learning_rate: float = 0.1, epochs: int = 1, training_inputs=None, training_outputs=None):
         for step in range(epochs):
-            self.weights += - learning_rate * self.gradient()[0]
-            self.biases += - learning_rate * self.gradient()[1]
+            for i,o in zip(training_inputs, training_outputs):
+                self.think(i)
+                grad = self.gradient(o)
+                self.weights += - learning_rate * grad[0]
+                self.biases += - learning_rate * grad[1]
 
 
-nn = NeuralNet(layer_sizes=[5, 4, 3, 2])
+# nn = NeuralNet(layer_sizes=[5, 4, 3, 2])
+#
+# input_data = np.array([[i for i in range(5)]])
 
-input_data = np.array([[i for i in range(5)]])
+training_inputs = np.array([[0,0,1],
+                            [1,1,1],
+                            [1,0,1],
+                            [0,1,1]])
 
-result = nn.think(input_data)
-print('result: ')
-print(result)
-print(nn.gradient())
+training_outputs = np.array([[0],
+                             [1],
+                             [1],
+                             [0]])
+
+# result = nn.think(input_data)
+# print('result: ')
+# print(result)
+
+small_network = NeuralNet(layer_sizes=[3,3,3,2,1])
+# small_network.learn(learning_rate=0.5, epochs=1, training_inputs=training_inputs, training_outputs=training_outputs)
+small_network.think(np.array([[1,1,1]]))
+# print(small_network.weights)
+print(small_network.gradient(1))
 
 # print('biases: ', nn.biases)
 # print('weights: ', nn.weights)
